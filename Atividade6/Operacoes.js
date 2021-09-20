1cca9117876cb85303707fd075ff3b0e2acf6643
xexeeeu <59447312+xexeeeu@users.noreply.github.com>
2021-09-20 12:18:20 -0300

    Add files via upload

---
+++ Atividade6/Operacoes.js
@@ -0,0 +1,13 @@
+var num1 = prompt('Digite o numero A:');
+var num1Num = parseFloat(num1);
+
+var num2 = prompt('Digite o numero B:');
+var num2Num = parseFloat(num2);
+
+alert('A + B = '+ (num1Num+num2Num));
+
+alert('A - B = '+ (num1Num-num2Num));
+
+alert('A * B = '+ (num1Num*num2Num));
+
+alert('A % B = '+ (num1Num%num2Num));
