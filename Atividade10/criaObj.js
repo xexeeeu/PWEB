559765bd7961923b0bb181255dbcd9d713189bc5
xexeeeu <59447312+xexeeeu@users.noreply.github.com>
2021-10-19 10:23:29 -0300

    Add files via upload

---
+++ Atividade10/criaObj.js
@@ -0,0 +1,13 @@
+function Aluno(ra,nome){
+    this.ra = 123;
+    this.nome = Felipe;
+}
+
+var Aluno = new Object();
+Aluno.ra = "123";
+Aluno.nome = "Felipe";
+
+var Aluno = {
+    ra:123,
+    nome:"Felipe"
+}
